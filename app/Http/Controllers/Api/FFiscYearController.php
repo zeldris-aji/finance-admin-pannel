245d95359498bc21fa57a1fa296c2322212d978a
ajithitrobes <ajith@itrobes.com>
2025-02-12 18:23:21 +0530

    added changes

--- app/Http/Controllers/Api/FFiscYearController.php
+++ app/Http/Controllers/Api/FFiscYearController.php
@@ -48,6 +48,7 @@ class FFiscYearController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    
     public function store(Request $request)
     {
         try {
